c87deb805f4521a55a1bc28af13fc0f5ff350a41
je9rojas <je9rojas@gmail.com>
2024-12-17 13:50:02 -0500

    v3.1

--- backend/models/Purchase.js
+++ backend/models/Purchase.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const purchaseSchema = new Schema({
   code: { type: String, required: true },          // Código del producto
   productType: { type: String, required: true },   // Tipo de producto
+  brand: { type: String, required: true },
   quantity: { type: Number, required: true },      // Cantidad del producto
   price: { type: Number, required: true },         // Precio del producto
   purchaseDate: { type: Date, required: true },    // Fecha de compra
